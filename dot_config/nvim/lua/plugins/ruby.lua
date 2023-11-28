659ce261c5dc2b62271486d1040b23adb29740a8
Martin Svangren <martin@masv.net>
2023-11-28 22:40:11 +0100

    Re-enable Solargraph, move LSP config

--- dot_config/nvim/lua/plugins/ruby.lua
+++ dot_config/nvim/lua/plugins/ruby.lua
@@ -3,10 +3,6 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        solargraph = {
-          mason = false,
-          autostart = false,
-        },
         standardrb = {},
       },
     },
