6df35bc89e2e2083da06f1fa3ea3bfddfb041804
Martin Svangren <martin@masv.net>
2023-12-01 16:30:46 +0100

    Disable Solargraph again, it's acting weird
    This reverts commit 659ce261c5dc2b62271486d1040b23adb29740a8.

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -3,6 +3,10 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
+        solargraph = {
+          mason = false,
+          autostart = false,
+        },
         standardrb = {},
       },
     },
