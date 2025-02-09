4d52f7ea9c8c181528e2988996fd36b1369cc9e1
Martin Svangren <martin@masv.net>
2025-02-09 16:47:10 +0100

    add zoxide

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -7,9 +7,10 @@ fish_add_path ~/bin
 fish_add_path ~/.asdf/shims
 fish_add_path /opt/homebrew/bin
 fish_add_path /opt/homebrew/sbin
-fish_add_path /opt/homebrew/opt/postgresql@12/bin
+fish_add_path /opt/homebrew/opt/postgresql@16/bin
 
 op completion fish | source
+zoxide init fish | source
 
 alias cz="chezmoi"
 alias l="eza -l --icons --git -a"
