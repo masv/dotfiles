62cc8b4ce6a9e48d2485efebff8bcd30aa12a3a1
Martin Svangren <martin@masv.net>
2025-02-10 22:10:05 +0100

    add neotest minitest adapter

--- dot_config/nvim/lua/plugins/ruby.lua
+++ dot_config/nvim/lua/plugins/ruby.lua
@@ -1,10 +1,23 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      ruby_lsp = {
-        mason = false,
-        cmd = { vim.fn.expand("~/.asdf/shims/ruby-lsp") },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        ruby_lsp = {
+          mason = false,
+          cmd = { vim.fn.expand("~/.asdf/shims/ruby-lsp") },
+        },
+      },
+    },
+  },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "zidhuss/neotest-minitest",
+    },
+    opts = {
+      adapters = {
+        "neotest-minitest",
       },
     },
   },
