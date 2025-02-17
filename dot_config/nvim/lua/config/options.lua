bd40adfb29c92bd33c595a83ec168f7680e5084c
Martin Svangren <martin@masv.net>
2025-02-17 21:44:07 +0100

    change nvim local leader

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -2,5 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.g.maplocalleader = ","
+
 vim.g.lazyvim_ruby_lsp = "ruby_lsp"
 vim.g.lazyvim_ruby_formatter = "standardrb"
