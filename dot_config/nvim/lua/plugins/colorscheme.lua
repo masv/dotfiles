8ffb144953892ac3cf63a56dd4bdb9fdf1345d64
Martin Svangren <martin@masv.net>
2025-09-06 18:57:28 +0200

    temp fix for nvim catppuccin

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -3,15 +3,24 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-    opts = {
-      flavour = "macchiato",
-    },
+    -- opts = {
+    --   flavour = "macchiato",
+    -- },
+    -- Temp fix: https://github.com/LazyVim/LazyVim/issues/6355#issuecomment-3212986215
+    -- Fix PR: https://github.com/LazyVim/LazyVim/pull/6354
+    opts = function(_, opts)
+      local module = require("catppuccin.groups.integrations.bufferline")
+      if module then
+        module.get = module.get_theme
+      end
+      return opts
+    end,
   },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "catppuccin-macchiato",
     },
   },
 }
