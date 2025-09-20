7fb5f0895791b74d938a19ecef36bc8ef2a3a3d8
Martin Svangren <martin@masv.net>
2025-09-20 12:00:57 +0200

    Remove nvim catppuccin fix
    This reverts commit 8ffb144953892ac3cf63a56dd4bdb9fdf1345d64.

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -3,24 +3,15 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-    -- opts = {
-    --   flavour = "macchiato",
-    -- },
-    -- Temp fix: https://github.com/LazyVim/LazyVim/issues/6355#issuecomment-3212986215
-    -- Fix PR: https://github.com/LazyVim/LazyVim/pull/6354
-    opts = function(_, opts)
-      local module = require("catppuccin.groups.integrations.bufferline")
-      if module then
-        module.get = module.get_theme
-      end
-      return opts
-    end,
+    opts = {
+      flavour = "macchiato",
+    },
   },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-macchiato",
+      colorscheme = "catppuccin",
     },
   },
 }
